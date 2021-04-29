8683a7e743b3bda75aa103fbb8a67a4cd3b09c75
Raghav <raghavarya2002@gmail.com>
2021-04-29 22:45:02 +0530

    new Adding Instagram Story functionality in story branch

--- app/src/main/java/com/example/instagram/Fragment/HomeFragment.kt
+++ app/src/main/java/com/example/instagram/Fragment/HomeFragment.kt
@@ -8,7 +8,9 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.instagram.Adapter.PostAdapter
+import com.example.instagram.Adapter.StoryAdapterr
 import com.example.instagram.Model.Post
+import com.example.instagram.Model.Story
 import com.example.instagram.R
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
@@ -20,7 +22,10 @@ import com.google.firebase.database.ValueEventListener
 class HomeFragment : Fragment() {
     private var postAdapter: PostAdapter? = null
     private var postList: MutableList<Post>? = null
-    private var followingList: MutableList<Post>? = null
+    private var followingList: MutableList<String>? = null
+
+    private var storyAdapterr : StoryAdapterr ? = null
+    private var storyList : MutableList<Story> ? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,6 +48,18 @@ class HomeFragment : Fragment() {
         linearLayoutManager.reverseLayout = true
         linearLayoutManager.stackFromEnd = true
         recyclerView.layoutManager = linearLayoutManager
+
+
+        var recyclerViewStory: RecyclerView? = null
+        recyclerViewStory = view.findViewById(R.id.recycler_view_story)
+        val linearLayoutManager2 = LinearLayoutManager(context)
+        linearLayoutManager2.reverseLayout = true
+        linearLayoutManager2.stackFromEnd = true
+        recyclerViewStory.layoutManager = linearLayoutManager2
+
+        storyList = ArrayList()
+        storyAdapterr = context?.let { StoryAdapterr(it,storyList as ArrayList<Story>) }
+        recyclerViewStory.adapter = storyAdapterr
 
         postList = ArrayList()
         postAdapter = context?.let { PostAdapter(it, postList as ArrayList<Post>) }
@@ -77,6 +94,7 @@ class HomeFragment : Fragment() {
                     }
 
                     retrievePosts()
+                    retrieveStories()
 
                 }
 
@@ -92,32 +110,89 @@ class HomeFragment : Fragment() {
     }
 
     private fun retrievePosts() {
-        val postsRef = FirebaseDatabase.getInstance().reference.child("Posts")
-        postsRef.addValueEventListener(object : ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                postList?.clear()
-                for (snapshot in snapshot.children) {
-                    val post = snapshot.getValue(Post::class.java)
-                    for (id in (followingList as ArrayList<String>)) {
-                        if (post!!.getPublisher().equals(id)) {
-                            postList!!.add(post)
+
+            val postsRef = FirebaseDatabase.getInstance().reference.child("Posts")
+            postsRef.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    postList?.clear()
+                    for (snapshot in snapshot.children) {
+                        val post = snapshot.getValue(Post::class.java)
+                        for (id in (followingList as ArrayList<String>)) {
+                            if (post!!.getPublisher().equals(id)) {
+                                postList!!.add(post)
+
+                            }
+                            postAdapter!!.notifyDataSetChanged()
 
                         }
-                        postAdapter!!.notifyDataSetChanged()
+
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+
+
+            })
+
+        }
+
+    private fun retrieveStories() {
+        val storyRef = FirebaseDatabase.getInstance().reference.child("story")
+        storyRef.addValueEventListener(object: ValueEventListener{
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                val timeCurrent = System.currentTimeMillis()
+
+                (storyList as ArrayList<Story>).clear()
+                (storyList as ArrayList<Story>).add(Story("",0,0,"",FirebaseAuth.getInstance().currentUser!!.uid))
+
+
+                for (id in followingList!!){
+
+                    var countStory = 0
+                    var story :Story ?= null
+
+                    for (snapshot in snapshot.child(id).children){
+
+                        story = snapshot.getValue(Story::class.java)
+
+                        if (timeCurrent>story!!.getTimeStart() && timeCurrent<story!!.getTimeEnd()){
+
+
+                            countStory++
+
+                        }
+
+                    }
+
+                    if (countStory > 0){
+
+                        (storyList as ArrayList<Story>).add(story!!)
 
                     }
 
                 }
-            }
 
-            override fun onCancelled(error: DatabaseError) {
-                TODO("Not yet implemented")
+                storyAdapterr!!.notifyDataSetChanged()
+
+
             }
 
 
         })
-
+    }
     }
 
 
-}
+
+
+
+
+
+
